f9dfd19653ec47e7ed06d507c53db74f89ddf597
Nathan Mc Grath <contact@nathan-mcgrath.com>
2019-10-06 01:00:42 +0200

    added todos empty state

--- apps/dodo/src/app/components/todo-list/todo-list.component.ts
+++ apps/dodo/src/app/components/todo-list/todo-list.component.ts
@@ -5,6 +5,7 @@ import { Todo } from '../../models';
 @Component({
   selector: 'nmg-todo-list',
   templateUrl: './todo-list.component.html',
+  styleUrls: ['./todo-list.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class TodoListComponent {
