5c9dbb10cb06c32092425026e9c05dc55d74a1c1
Nathan Mc Grath <contact@nathan-mcgrath.com>
2019-10-06 03:45:11 +0200

    added logo to header

--- apps/dodo/src/app/components/header/header.component.ts
+++ apps/dodo/src/app/components/header/header.component.ts
@@ -3,6 +3,7 @@ import { Component, ChangeDetectionStrategy } from '@angular/core';
 @Component({
   selector: 'nmg-header',
   templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class HeaderComponent {}
