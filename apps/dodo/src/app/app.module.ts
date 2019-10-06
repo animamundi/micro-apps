86e7a82616dc4e628293e2bf386d38bbe28582e2
Nathan Mc Grath <contact@nathan-mcgrath.com>
2019-10-06 03:08:20 +0200

    added pwa features

--- apps/dodo/src/app/app.module.ts
+++ apps/dodo/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
+import { ServiceWorkerModule } from '@angular/service-worker';
 
 import { AppComponent } from './app.component';
 import { AppStoreModule } from './app-store.module';
@@ -9,6 +10,7 @@ import {
   AddTodoContainerModule,
   HeaderModule,
 } from './components';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [AppComponent],
@@ -19,6 +21,9 @@ import {
     HeaderModule,
     TodoListContainerModule,
     AddTodoContainerModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: environment.production,
+    }),
   ],
   bootstrap: [AppComponent],
 })
