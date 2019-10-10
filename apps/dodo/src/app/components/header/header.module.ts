a4c3e5b0da54cd32c6f74922cceed5aad03301aa
Nathan Mc Grath <contact@nathan-mcgrath.com>
2019-10-10 22:44:11 +0200

    put sign out button into menu

--- apps/dodo/src/app/components/header/header.module.ts
+++ apps/dodo/src/app/components/header/header.module.ts
@@ -1,12 +1,24 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { MatToolbarModule, MatButtonModule } from '@angular/material';
+import {
+  MatToolbarModule,
+  MatButtonModule,
+  MatIconModule,
+  MatMenuModule,
+} from '@angular/material';
 import { RouterModule } from '@angular/router';
 
 import { HeaderComponent } from './header.component';
 
 @NgModule({
-  imports: [CommonModule, RouterModule, MatToolbarModule, MatButtonModule],
+  imports: [
+    CommonModule,
+    RouterModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule,
+    MatMenuModule,
+  ],
   declarations: [HeaderComponent],
   exports: [HeaderComponent],
 })
